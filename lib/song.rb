20174169de6fbb1f448d0314179320405826f6a3
ali aziz <aa_connect@outlook.com>
2018-07-24 16:29:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 class Song
   
   @@count = 0
+  @@artist = 0
   
   attr_accessor :name, :artist, :genre
   
@@ -9,6 +10,7 @@ class Song
     @artist = artist
     @genre = genre
     @@count +=1
+    @@artist << @artist
   end
   
 end

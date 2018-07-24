eec205ba8e5c6024f105f49b19b3cb280cd5ea7d
ali aziz <aa_connect@outlook.com>
2018-07-24 16:32:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   
   @@count = 0
-  @@artist = []
+  @@artists = []
   
   attr_accessor :name, :artist, :genre
   
@@ -10,7 +10,7 @@ class Song
     @artist = artist
     @genre = genre
     @@count +=1
-    @@artist << @artist
+    @@artists << @artist
   end
   
 end

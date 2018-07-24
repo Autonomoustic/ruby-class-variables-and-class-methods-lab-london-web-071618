50a5645907faeaf4e128941783a7915b6a2dcb68
ali aziz <aa_connect@outlook.com>
2018-07-24 17:00:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,6 +34,13 @@ class Song
   def self.genre_count(genre)
     genre_count = {}
     @@genres.each do |g|
+      #check if it's already in hash
+      if genre_count != g
+        genre_count[g] = 1
+      else
+        genre_count[g] += 1
+      #if no add new key with the value
+      #if yes iterate/ add 1 to count
     end
     genre_count
   end
